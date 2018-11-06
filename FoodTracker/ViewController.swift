02f282ace47768d7745b1373de9b06394bc44f69
Michelle Mallixi <michellemallixi@ADC-RyanGonzalez.local>
2018-11-06 10:22:40 +0800

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Michelle Mallixi on 11/6/18.
+//  Copyright © 2018 Michelle Malixi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
